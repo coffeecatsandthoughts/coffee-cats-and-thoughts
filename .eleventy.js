7b7a3ed12bdc658fe6caf883a664c1eff18c5a35
coffeecatsandthoughts <jvillanueva1229@gmail.com>
2026-09-04 16:25:16 -0600

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
 
+  eleventyConfig.addFilter("readableDate", function(date) {
+    return new Intl.DateTimeFormat("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      timeZone: "UTC"
+    }).format(date);
+  });
+
   return {
     dir: {
       input: ".",
